2fba5e23d520743dab60c3fbf65c401d8d64a526
Dragan Glumac <dragan.glumac@gmail.com>
2015-04-03 10:29:34 +0100

    More Heap stuff

--- week1/prim/src/main/java/uk/co/gluedit/Heap.java
+++ week1/prim/src/main/java/uk/co/gluedit/Heap.java
@@ -1,23 +1,66 @@
 package uk.co.gluedit;
 
-import java.util.Collection;
+import java.util.ArrayList;
 
-public class Heap<Comparable> {
+public class Heap<T extends Comparable<T>> {
+    ArrayList<T> pq;
+
+    public Heap() {
+        pq = new ArrayList<>();
+    }
 
     public boolean isEmpty() {
-        return true;
+        return pq.isEmpty();
     }
-    public void insert(Comparable el) {
+
+    public void insert(T el) {
+        pq.add(el);
+        bubbleUp();
+    }
+
+    private void bubbleUp() {
+        int index = pq.size() - 1; =
+        int parent = parentForIndex(index);
+        while (index > 0) {
+            if (0 < pq.get(parent).compareTo(pq.get(index))) {
+                swapElementsAt(parent, index);
+                index = parent;
+                parent = parentForIndex(index);
+                continue;
+            }
+            break;
+        }
+    }
+
+    private void bubbleDown() {
         // TODO
     }
-    public void heapify(Collection<Comparable> col) {
-        // TODO
+    private void swapElementsAt(int parent, int index) {
+        T temp = pq.get(index);
+        pq.add(index, pq.get(parent));
+        pq.add(parent, temp);
     }
-    public Comparable extractMin() {
-        // TODO
-        return null;
+
+    private int parentForIndex(int index) {
+        if (index <= 0)
+            return 0;
+        return (index - 1) / 2;
     }
-    public void remove(Comparable el) {
+
+    public void heapify(T[] col) {
+        for (T el : col)
+            insert(el);
+    }
+
+    public T extractMin() {
+        if (isEmpty()) return null;
+        T min = pq.get(0);
+        pq.add(0, pq.remove(pq.size() - 1));
+        bubbleDown();
+        return min;
+    }
+
+    public void remove(T el) {
         // TODO
     }
 }
--- week1/prim/src/test/java/co/uk/gluedit/HeapTest.java
+++ week1/prim/src/test/java/co/uk/gluedit/HeapTest.java
@@ -7,11 +7,12 @@ import org.junit.runner.RunWith;
 import uk.co.gluedit.Heap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(HierarchicalContextRunner.class)
 public class HeapTest {
-    private Heap<Integer> heap;
+    private Heap<Integer>heap;
 
     @Before public void setUp() {
         heap = new Heap<>();
@@ -19,5 +20,45 @@ public class HeapTest {
     @Test public void testEmptyHeap() {
         assertTrue(heap.isEmpty());
         assertEquals(heap.extractMin(), null);
+    }
+    @Test public void testSingleElement() {
+        heap.insert(42);
+        assertFalse(heap.isEmpty());
+        assertEquals(heap.extractMin(), new Integer(42));
+    }
+    private void assertOrder(Integer[] original, Integer[] order) {
+        heap.heapify(original);
+        for (Integer i : order)
+            assertEquals(i, heap.extractMin());
+    }
+    @Test public void tesHeapify() {
+        Integer[] nums = {1, 2, 3};
+        assertOrder(nums, nums);
+
+        Integer[] nums2 = {2, 1, 3};
+        assertOrder(nums2, nums);
+
+        Integer[] nums3 = {1, 3, 2};
+        assertOrder(nums3, nums);
+
+        Integer[] nums4 = {2, 3, 1};
+        assertOrder(nums4, nums);
+
+        Integer[] nums5 = {3, 1, 2};
+        assertOrder(nums5, nums);
+
+        Integer[] nums6 = {3, 2, 1};
+        assertOrder(nums6, nums);
+    }
+    @Test public void testTwoElements() {
+        heap.insert(1);
+        heap.insert(2);
+        assertEquals(heap.extractMin(), new Integer(1));
+        assertEquals(heap.extractMin(), new Integer(2));
+
+        heap.insert(2);
+        heap.insert(1);
+        assertEquals(heap.extractMin(), new Integer(1));
+        assertEquals(heap.extractMin(), new Integer(2));
     }
 }
